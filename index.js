a1c671d2a6e3fb0d6637e27a62ef39f979e5e92c
Jeferson <yefersonardila100@gmail.com>
2025-03-01 16:36:35 -0500

    create main folder and files

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
 app.use(express.json());
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT, ()=>{
+    console.log(`Servidor Funcionando en http://localhost:${PORT}`);
+});
