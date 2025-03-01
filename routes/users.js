0ee1db961e2839717d2b569962e7dccba8f49baf
Jeferson <yefersonardila100@gmail.com>
2025-03-01 18:44:39 -0500

    add update users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getUsers, createUser } = require("../controllers/users");
+const { getUsers, createUser,updateUser} = require("../controllers/users");
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", createUser);
+router.put("/users/:id", updateUser);
 /* router.get("/books/:id", getBookById);
 router.post("/books", validateBook, createBook);
 router.put("/books/:id", validateBook, updateBook);
