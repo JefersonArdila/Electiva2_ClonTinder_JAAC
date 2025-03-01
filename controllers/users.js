5e6b7a3a363533d98e1c81f1dd5a60d2ebbd2d8f
Jeferson <yefersonardila100@gmail.com>
2025-03-01 18:44:39 -0500

    add update users route

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const getUsers = (req, res) =>{
     res.status(200).json(users);
 };
 
+
 const createUser = (req, res) => {
     const { firstName, lastName, age, email, gender } = req.body;
       const newUser = {
@@ -19,7 +20,33 @@ const createUser = (req, res) => {
       res.status(201).json(newUser);
 };
 
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);  
+    const { firstName, lastName, age, email, gender } = req.body; 
+    
+    
+    const userIndex = users.findIndex(u => u.id === id);
+    
+    
+    if (userIndex === -1) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+    
+    users[userIndex] = {
+        ...users[userIndex],
+        firstName: firstName || users[userIndex].firstName,
+        lastName: lastName || users[userIndex].lastName,
+        age: age || users[userIndex].age,
+        email: email || users[userIndex].email,
+        gender: gender || users[userIndex].gender
+    };
+    
+    res.json(users[userIndex]);
+};
+
 module.exports = {
   getUsers,
   createUser,
+  updateUser
 };
