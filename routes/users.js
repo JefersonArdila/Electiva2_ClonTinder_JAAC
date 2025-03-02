3b00d0b6992dc5590fcfa1a33447c3031ee73a4c
Johan Alexander Rojas Suarez <JohanRojas@comfama.com.co>
2025-03-01 19:12:13 -0500

    add get user by id and delete routes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const { getUsers, createUser,updateUser} = require("../controllers/users");
+const { getUsers, getUserId, createUser, updateUser, deleteUser } = require("../controllers/users");
 const router = express.Router();
 
 router.get("/users", getUsers);
+router.get("/users/:id", getUserId);
 router.post("/users", createUser);
 router.put("/users/:id", updateUser);
-/* router.get("/books/:id", getBookById);
+router.delete("/users/:id", deleteUser);
+/*
 router.post("/books", validateBook, createBook);
 router.put("/books/:id", validateBook, updateBook);
-router.delete("/books/:id", deleteBook); */
+*/
 
 module.exports = router;
