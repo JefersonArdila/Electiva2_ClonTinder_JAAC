3d59e5ce237fbc1a945ff0528e8cac6417cf2cb9
Jeferson <yefersonardila100@gmail.com>
2025-03-01 21:58:17 -0500

    add create routes and files the matches and swipes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const swipeRoutes  = require('./routes/swipes');
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
-app.use("/api", userRouter, authRouter);
+app.use("/api", userRouter, authRouter,swipeRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Servidor Funcionando en http://localhost:${PORT}`);
