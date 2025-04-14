d50c118dd84d7b5a74c31aec6ec5d45684432867
Johan Alexander Rojas Suarez <JohanRojas@comfama.com.co>
2025-04-14 18:11:38 -0500

    chg dotenv config and update gitignore

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const swipeRoutes = require("./routes/swipes");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 app.use(express.json());
 app.use("/api", userRoutes, authRoutes, swipeRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor Funcionando en http://localhost:${PORT}`);
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
