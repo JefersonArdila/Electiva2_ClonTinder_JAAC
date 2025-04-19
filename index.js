766edb2000fae9db766cec3e06f3cd4f708dd415
Johan Rojas <jrojass@correo.tdea.edu.co>
2025-04-19 11:33:21 -0500

    add auth middleware and register endpoint

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const swipeRoutes = require("./routes/swipes");
-const { PrismaClient } = require("@prisma/client");
+const authenticationToken = require("./middlewares/auth");
+const { PrismaClient } = require("./generated/prisma/client.js");
 const prisma = new PrismaClient();
 const app = express();
 
@@ -13,7 +14,7 @@ console.log(PORT);
 app.use(express.json());
 app.use("/api", userRoutes, authRoutes, swipeRoutes);
 
-// Endpoint para comprobar la conexión a la base de datos
+// Endpoint temporal para comprobar la conexión a la base de datos
 app.get("/db-users", async (req, res) => {
   try {
     const users = await prisma.user.findMany();
@@ -21,6 +22,11 @@ app.get("/db-users", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Error al comunicarse con la base de datos" });
   }
+});
+
+// Endpoint temporal para comprobar la protección de rutas
+app.get("/protected-route", authenticationToken, (req, res) => {
+  res.send("Esta es una ruta protegida");
 });
 
 app.listen(PORT, () => {
