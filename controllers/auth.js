d935beba9cfb568ecd507409acd76e80f44b38b7
Johan Rojas <jrojass@correo.tdea.edu.co>
2025-04-19 20:17:07 -0500

    chg login user with db validation and token

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,16 +20,29 @@ const registerUser = async (req, res) => {
   res.status(201).json({ message: "Usuario registrado correctamente" });
 };
 
-const loginUser = (req, res) => {
-    const credentials = {
-        user: "admin",
-        password: "admin",
-    };
-  const { user, password } = req.body;
-  if (user !== credentials.user || password !== credentials.password) {
-    return res.status(401).json({ error: "Credenciales incorrectas" });
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  // Buscar el usuario en la base de datos
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(400).json({ error: "Usuario o contraseña inválida" });
   }
-  res.status(200).json({ message: "Bienvenido " + user });
+  // Verificar la contraseña
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return res.status(400).json({ error: "Usuario o contraseña inválida" });
+  }
+  // Generar un token JWT
+  const token = jwt.sign(
+    { id: user.id }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: "4h" }
+  );
+  res.json({
+    message: "Inicio de sesión exitoso",
+    token,
+    user
+  });
 };
 
 const logoutUser = (req, res) => {
